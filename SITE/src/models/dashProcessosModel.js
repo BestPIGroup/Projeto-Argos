902b1aca3ae7c9175ebbd97b23f6fe22c99881c9
LUA-CHAVES-SPT <lua.chaves@sptech.school>
2026-06-02 15:06:11 -0300

    Inicio da rota, caminho reconhecido , mas dados estao errados

---
+++ SITE/src/models/dashProcessosModel.js
@@ -0,0 +1,12 @@
+const AWS = require("aws-sdk");
+const fs = require('fs');
+const readline = require('readline');
+
+const express = require("express");
+const router = express.Router();
+
+async function buscarDados(mac, linhas){
+
+    console.log(AWS)
+    
+}
