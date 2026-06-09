613e179f53bef6713e3040905a48ff8c19054d20
ak-miyuki <andreia.kubota@sptech.school>
2026-06-09 20:13:19 -0300

    .env.dev

--- SITE/app.js
+++ SITE/app.js
@@ -1,5 +1,5 @@
-const ambiente_processo = "producao";
-//const ambiente_processo = "desenvolvimento";
+// const ambiente_processo = "producao";
+const ambiente_processo = "desenvolvimento";
 const caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
 
 require("dotenv").config({ path: caminho_env });
